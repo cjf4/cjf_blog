25b0ab4614c4b5bb8e3a64b8616f066b1b060b41
Chris Fenton <christopherfenton4@gmail.com>
2018-04-04 22:58:32 -0400

    first migrations

--- cjf_blog/reviews/models.py
+++ cjf_blog/reviews/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Review(models.Model):
+	pub_date = models.DateTimeField('publication date')
+	score = models.DecimalField(max_digits = 3, decimal_places=1)
+	subject_name = models.CharField(max_length = 30)
+	review_summary = models.CharField(max_length = 50)
+	category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
+
+class Category(models.Model):
+	category = models.CharField(max_length = 30)
+
